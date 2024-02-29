4801fe71eaa541f4842a7fd951a16ceb0b49bb84
Wesley Santos <wesleysantos32892653@outlook.com>
2024-02-29 00:27:20 -0300

    Finalizando lista

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -6,6 +6,10 @@ import { ITask } from "../../interfaces/Task";
 // CSS
 import "./TaskList.css";
 
+// Icons
+import { FaPencilAlt } from "react-icons/fa";
+import { CiTrash } from "react-icons/ci";
+
 type Props = {
   taskList: ITask[];
 };
@@ -15,8 +19,15 @@ const TaskList = ({ taskList }: Props) => {
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            <p>{task.title}</p>
+          <div key={task.id} className="task">
+            <div className="details">
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className="actions">
+              <FaPencilAlt className="icon" />
+              <CiTrash className="icon" />
+            </div>
           </div>
         ))
       ) : (
